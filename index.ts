f75fb6aa5bb0f5d2ef5e348f20142e83bf299160
Antoine Cottineau <antoine.cottineau.dev@gmail.com>
2023-12-04 10:34:52 +0100

    feat: start implementing css algorithm

--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
 export { interpolateColor } from "./src/interpolate";
 export { deltaOK } from "./src/distance";
+
+import { Color } from "./src/colors";
+import { CSS4GamutMapping } from "./src/gamut/rgb/cssInterpolation";
+
+const origin = new Color.RGB(0.5, 1.2, 1.4);
+const gammutAlgorithm = new CSS4GamutMapping()
+const gammutMappedColor = gammutAlgorithm.visitRGBColor(origin)
+console.log("gammutMappedColor", gammutMappedColor)
