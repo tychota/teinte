99e425cd42d10b7d950fb4ab986a3e6ff307c2aa
Antoine Cottineau <antoine.cottineau.dev@gmail.com>
2023-12-04 10:34:52 +0100

    feat: start implementing css algorithm

---
+++ src/gamut/rgb/cssInterpolation.test.ts
@@ -0,0 +1,39 @@
+import { describe, it, expect } from "bun:test";
+
+import { CSS4GamutMapping } from "./cssInterpolation";
+import { Color } from "../../colors";
+
+const gammutAlgorithm = new CSS4GamutMapping();
+
+describe("matrixMultiplication", () => {
+  it("should return the same color if passed an RGB color", () => {
+    // Given
+    const origin = new Color.RGB(0.5, 0.5, 0.5);
+    // When
+    const gammutMappedColor = gammutAlgorithm.visitRGBColor(origin);
+    // Then
+    expect(gammutMappedColor.r).toBeCloseTo(0.5);
+    expect(gammutMappedColor.g).toBeCloseTo(0.5);
+    expect(gammutMappedColor.b).toBeCloseTo(0.5);
+  });
+  it("should return RGB(1,1,1) if oklch lightness is more than 1", () => {
+       // Given
+       const origin = new Color.RGB(0.5, 1.2, 1.4);
+       // When
+       const gammutMappedColor = gammutAlgorithm.visitRGBColor(origin);
+       // Then
+       expect(gammutMappedColor.r).toBeCloseTo(1);
+       expect(gammutMappedColor.g).toBeCloseTo(1);
+       expect(gammutMappedColor.b).toBeCloseTo(1);
+  });
+  it("should return RGB(0,0,0) if oklch lightness is less than 0", () => {
+           // Given
+           const origin = new Color.RGB(-0.5, 0.1, -0.4);
+           // When
+           const gammutMappedColor = gammutAlgorithm.visitRGBColor(origin);
+           // Then
+           expect(gammutMappedColor.r).toBeCloseTo(0);
+           expect(gammutMappedColor.g).toBeCloseTo(0);
+           expect(gammutMappedColor.b).toBeCloseTo(0);
+  });
+});
---
+++ src/gamut/rgb/cssInterpolation.ts
@@ -0,0 +1,120 @@
+import { Color } from "../../colors";
+import { ToOkLabColorspaceVisitor } from "../../colorspace/oklab";
+import { ToOkLCHColorspaceVisitor } from "../../colorspace/oklch";
+import { ToRGBColorspaceVisitor } from "../../colorspace/rgb";
+
+export class CSS4GamutMapping extends ToRGBColorspaceVisitor {
+  rgbVisitor = new ToRGBColorspaceVisitor();
+  oklabVisitor  = new ToOkLabColorspaceVisitor();
+  oklchVisitor = new ToOkLCHColorspaceVisitor();
+
+  public visitRGBColor(color: InstanceType<typeof Color.RGB>) {
+    // We follow algorithm https://www.w3.org/TR/css-color-4/#binsearch
+
+    const origin = color;
+
+    // Step 1 : does not apply as RGB has gamut limit
+    // const nogammutLimitColorspaces = [Color.XYZ, Color.OkLCH, Color.OkLab] as const
+    // const colorspaceHasGamutLimit = nogammutLimitColorspaces.includes(destination)
+    const colorspaceHasGamutLimit = false;
+    if (colorspaceHasGamutLimit) {
+      return color;
+    }
+
+    // Step 2 : create the color in the OKLch color space
+    const origin_Oklch = this.oklchVisitor.visitRGBColor(origin);
+
+    console.log("Step 1", origin_Oklch);
+
+    // Step 3 : if the Lightness of origin_Oklch is greater than or equal to 100%, return { 1 1 1 } in destination
+    // Note the algorithm speak about alpha but we don't support alpha
+    if (origin_Oklch.l >= 1) {
+      return new Color.RGB(1, 1, 1);
+    }
+
+    // Step 4 : if the Lightness of origin_Oklch is less than than or equal to 0%, return { 0 0 0 } in destination
+    // Note the algorithm speak about alpha but we don't support alpha
+    if (origin_Oklch.l <= 0) {
+      return new Color.RGB(0, 0, 0);
+    }
+
+    // Step 5 : let inGamut(color) be a function which returns true if, when passed a color, that color is inside the gamut of destination. For HSL and HWB, it returns true if the color is inside the gamut of sRGB.
+    const inGamut = (color: Color) => {
+      const rgbColor = color.accept(this.rgbVisitor) as InstanceType<typeof Color.RGB>;
+      return (
+        rgbColor.r >= 0 && rgbColor.r <= 1 && rgbColor.g >= 0 && rgbColor.g <= 1 && rgbColor.b >= 0 && rgbColor.b <= 1
+      );
+    };
+
+    // Step 6 : if inGamut(origin_Oklch) is true, convert origin_Oklch to destination and return it as the gamut mapped color
+    if (inGamut(origin_Oklch)) {
+      return origin_Oklch.accept(this.rgbVisitor) as InstanceType<typeof Color.RGB>;
+    } 
+    
+    // Step 7 : otherwise, let delta(one, two) be a function which returns the deltaEOK of color one compared to color two
+    else {
+      const delta = (one: Color, two: Color) => {
+        const oneOklab =  one.accept(this.oklabVisitor) as InstanceType<typeof Color.OkLab>;
+        const twoOklab =  two.accept(this.oklabVisitor) as InstanceType<typeof Color.OkLab>;
+
+        const delta_L = oneOklab.l - twoOklab.l;
+
+        const chroma1 = Math.sqrt(oneOklab.a * oneOklab.a + oneOklab.b * oneOklab.b);
+        const chroma2 = Math.sqrt(twoOklab.a * twoOklab.a + twoOklab.b * twoOklab.b);
+        const delta_C = chroma1 - chroma2;
+      
+        const delta_a = oneOklab.a - twoOklab.a;
+        const delta_b = oneOklab.b - twoOklab.b;
+        
+        const delta_H = Math.sqrt(delta_a * delta_a + delta_b * delta_b - delta_C * delta_C);
+      
+        return Math.sqrt(delta_L * delta_L + delta_C * delta_C + delta_H * delta_H);
+      }
+
+      // Step 8: let JND be 0.02
+      let JND = 0.02
+
+      // Step 9: let epsilon be 0.0001
+      let epsilon = 0.0001
+
+      // Step 10: let clip(color)| be a function which converts color to destination, 
+      // converts all negative components to zero, converts all components greater that 
+      // one to one, and returns the result
+      const clip = (color: Color) => {
+        const destinationColor = color.accept(this.rgbVisitor) as InstanceType<typeof Color.RGB>;
+
+        function clip(value: number, min: number, max: number) {
+          return Math.min(Math.max(value, min), max);
+        }
+
+        return new Color.RGB(
+          clip(destinationColor.r, 0, 1),
+          clip(destinationColor.g, 0, 1),
+          clip(destinationColor.b, 0, 1)
+        )
+      }
+
+
+    }
+
+
+
+    return color;
+  }
+  public visitHSLColor(color: InstanceType<typeof Color.HSL>) {
+    const rgb = this.rgbVisitor.visitHSLColor(color);
+    return this.visitRGBColor(rgb);
+  }
+  public visitXYZColor(color: InstanceType<typeof Color.XYZ>) {
+    const rgb = this.rgbVisitor.visitXYZColor(color);
+    return this.visitRGBColor(rgb);
+  }
+  public visitOkLabColor(color: InstanceType<typeof Color.OkLab>) {
+    const rgb = this.rgbVisitor.visitOkLabColor(color);
+    return this.visitRGBColor(rgb);
+  }
+  public visitOkLCHColor(color: InstanceType<typeof Color.OkLCH>) {
+    const rgb = this.rgbVisitor.visitOkLCHColor(color);
+    return this.visitRGBColor(rgb);
+  }
+}
