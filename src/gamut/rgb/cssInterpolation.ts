244ed7167749f1b95ba3b0d774cbd4a703eeff06
Tycho Tatitscheff <tychot@bam.tech>
2023-12-12 14:42:05 +0100

    refactor: clean up cssInterpolation

--- src/gamut/rgb/cssInterpolation.ts
+++ src/gamut/rgb/cssInterpolation.ts
@@ -23,35 +23,34 @@ export class CSS4GamutMapping extends ToRGBColorspaceVisitor {
     const origin = color;
 
     // Step 1 : if destination has no gamut limits (XYZ-D65, XYZ-D50, Lab, LCH, Oklab, Oklch) return origin
-    // does not apply as RGB has gamut limit
-    // const nogammutLimitColorspaces = [Color.XYZ, Color.OkLCH, Color.OkLab] as const
-    // const colorspaceHasGamutLimit = nogammutLimitColorspaces.includes(destination)
-    const colorspaceHasGamutLimit = false;
-    if (colorspaceHasGamutLimit) {
+    // => does not apply as RGB has gamut limit
+    const isColorspaceUnbounded = false;
+    if (isColorspaceUnbounded) {
       return color;
     }
 
     // Step 2 : create the color in the OKLch color space
     const origin_Oklch = this.oklchVisitor.visitRGBColor(origin);
+    const L = origin_Oklch.l;
 
     // Step 3 : if the Lightness of origin_Oklch is greater than or equal to 100%, return { 1 1 1 } in destination
-    // Note the algorithm speak about alpha but we don't support alpha
-    if (origin_Oklch.l >= 1) {
+    // (Note the algorithm speak about alpha but we don't support alpha)
+    if (L >= 1) {
       return new Color.RGB(1, 1, 1);
     }
 
     // Step 4 : if the Lightness of origin_Oklch is less than than or equal to 0%, return { 0 0 0 } in destination
-    // Note the algorithm speak about alpha but we don't support alpha
-    if (origin_Oklch.l <= 0) {
+    // (Note the algorithm speak about alpha but we don't support alpha)
+    if (L <= 0) {
       return new Color.RGB(0, 0, 0);
     }
 
-    // Step 5 : let inGamut(color) be a function which returns true if, when passed a color, that color is inside the gamut of destination. For HSL and HWB, it returns true if the color is inside the gamut of sRGB.
+    // Step 5 : let inGamut(color) be a function which returns true if, when passed a color, that color is inside the gamut of destination.
+    // For HSL and HWB, it returns true if the color is inside the gamut of sRGB.
     const inGamut = (color: Color) => {
       const rgbColor = color.accept(this.rgbVisitor) as InstanceType<typeof Color.RGB>;
-      return (
-        rgbColor.r >= 0 && rgbColor.r <= 1 && rgbColor.g >= 0 && rgbColor.g <= 1 && rgbColor.b >= 0 && rgbColor.b <= 1
-      );
+      const { r, g, b } = rgbColor;
+      return [r, g, b].every((c) => c >= 0 && c <= 1);
     };
 
     // Step 6 : if inGamut(origin_Oklch) is true, convert origin_Oklch to destination and return it as the gamut mapped color
@@ -77,10 +76,7 @@ export class CSS4GamutMapping extends ToRGBColorspaceVisitor {
     // one to one, and returns the result
     const clip = (color: Color) => {
       const destinationColor = color.accept(this.rgbVisitor) as InstanceType<typeof Color.RGB>;
-
-      const clippedColor = new Color.RGB(destinationColor.r, destinationColor.g, destinationColor.b);
-      clippedColor.clip();
-
+      const clippedColor = destinationColor.clone().clip();
       return clippedColor;
     };
 
@@ -93,7 +89,7 @@ export class CSS4GamutMapping extends ToRGBColorspaceVisitor {
     // Step 13: let min_inGamut be a boolean that represents when min is still in gamut, and set it to true
     let min_inGamut = true;
 
-    let current = new Color.OkLCH(origin_Oklch.l, origin_Oklch.c, origin_Oklch.h);
+    let current: InstanceType<typeof Color.OkLCH> = origin_Oklch;
 
     // Step 14: while (max - min is greater than epsilon) repeat the following steps
     while (max - min > epsilon) {
@@ -106,13 +102,12 @@ export class CSS4GamutMapping extends ToRGBColorspaceVisitor {
       // Step 14.3 if min_inGamut is true and also if inGamut(current) is true,
       // set min to chroma and continue to repeat these steps
       if (min_inGamut && inGamut(current)) {
-        console.log("min_inGamut && inGamut(current)", color);
         min = chroma;
         continue;
       }
 
       // Step 14.4: otherwise, if inGamut(current) is false carry out these steps
-      if (inGamut(current) === false) {
+      else if (!inGamut(current)) {
         // Step 14.4.1 : set clipped to clip(current)
         const clipped = clip(current);
 
